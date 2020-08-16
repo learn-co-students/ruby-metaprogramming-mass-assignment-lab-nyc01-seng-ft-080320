37fcb23daa0a6b87e42ac42b473766e571303a31
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-08-16 11:44:07 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,39 @@
 class Person
-  #your code here
+  # attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+  def initialize(attributes)
+    attributes.each do |k, v|
+      self.class.attr_accessor(k)
+      self.send(("#{k}="), v)
+    end
+  end
+  # def initialize(name:, birthday:,hair_color:,eye_color:,height:,weight:, handed:, complexion:, t_shirt_size:, wrist_size:, glove_size:, pant_length:, pant_width:)
+  #   @name = name
+  #   @birthday = birthday
+  #   @hair_color = hair_color
+  #   @eye_color = eye_color
+  #   @height = height
+  #   @weight = weight
+  #   @handed = handed
+  #   @complexion = complexion
+  #   @t_shirt_size = t_shirt_size
+  #   @wrist_size = wrist_size
+  #   @glove_size = glove_size
+  #   @pant_length = pant_length
+  #   @pant_width = pant_width
+  # end
 end
+
+avi_attributes = {:name => "Avi",
+  :birthday => "01/29/1984",
+  :hair_color => "brown",
+  :eye_color => "brown",
+  :height => "short",
+  :weight => "good",
+  :handed => "lefty",
+  :complexion => "decent",
+  :t_shirt_size => "medium",
+  :wrist_size => "small",
+  :glove_size => "normal",
+  :pant_length => "32",
+  :pant_width => "32"}
+avi = Person.new(avi_attributes)
